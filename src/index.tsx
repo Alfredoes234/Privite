b2e97b04a64b648cf2c2be64955e91171ef7718c
Alfredoes234 <alfie.rayner30@gmail.com>
2024-01-09 09:13:09 +0000

    added folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { render } from 'preact';
 import { LocationProvider, Router, Route } from 'preact-iso';
 
-import { Header } from './components/Header.jsx';
+import { Header } from './components/Header/Header.js';
 import { Home } from './pages/Home/index.jsx';
 import { NotFound } from './pages/_404.jsx';
 import './global.scss';
