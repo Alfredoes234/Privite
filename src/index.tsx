e6d336ffab9753f48d31c7c77d87ecb19b07e8f3
Alfredoes234 <alfie.rayner30@gmail.com>
2024-01-25 11:59:40 +0000

    added head details to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from 'preact';
 import { LocationProvider, Router, Route } from 'preact-iso';
-import Helmet from "preact-helmet";
+import { Helmet } from "preact-helmet";
 
 import { Header } from './components/Header/Header';
 import { Home } from './pages/Home/index';
@@ -11,7 +11,7 @@ import './global.css';
 export function App() {
 	return (
 		<LocationProvider>
-			<Helmet titleTemplate="Preact | %s" htmlAttributes={{ lang: "en", amp: undefined }} />
+			<Helmet titleTemplate="Preact | %s" htmlAttributes={{ lang: "en", amp: undefined }} meta={[{ name: "viewport", content: "width=device-width, initial-scale=1.0" }, { name: "color-scheme", content: "light dark" }]} />
 			<Header />
 			<main>
 				<Router>
